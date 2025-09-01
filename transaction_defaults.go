6d3ff03507a8fe4337bdce79b0fb183191e491d3
Vinicius Malloni <vinicius.malloni@bleu.com.br>
2025-09-01 13:33:43 -0300

    rename arguments of default transactions to better descriptions

--- transaction_defaults.go
+++ transaction_defaults.go
@@ -20,49 +20,49 @@ func TransferBetweenDebitAccounts(externalID string, fromAccountID, toAccountID 
 	return NewTransaction(nil).
 		WithExternalID(externalID).
 		AddEntry(fromAccountID, Credit, Debit, amount, currency).
-		AddEntry(toAccountID, Debit, Credit, amount, currency).
+		AddEntry(toAccountID, Debit, Debit, amount, currency).
 		MustBuild()
 }
 
 // Deposit creates a transaction for depositing an amount into a credit account from a cash account with debit normal side.
-func Deposit(externalID string, cashAccountID, toAccountID uuid.UUID, amount *big.Int, currency Currency) *Transaction {
+func Deposit(externalID string, debitAccountID, toCreditAccountID uuid.UUID, amount *big.Int, currency Currency) *Transaction {
 	return NewTransaction(nil).
 		WithExternalID(externalID).
-		AddEntry(cashAccountID, Debit, Debit, amount, currency).
-		AddEntry(toAccountID, Credit, Credit, amount, currency).
+		AddEntry(debitAccountID, Debit, Debit, amount, currency).
+		AddEntry(toCreditAccountID, Credit, Credit, amount, currency).
 		MustBuild()
 }
 
 // DepositWithFee its like Deposit but takes part of the amount as fee which is transferred to a fee account.
-func DepositWithFee(externalID string, cashAccountID, feeAccount, toAccountID uuid.UUID, feeTax int8, amount *big.Int, currency Currency) *Transaction {
+func DepositWithFee(externalID string, debitAccountID, creditFeeAccountID, toCreditAccountID uuid.UUID, feeTax int8, amount *big.Int, currency Currency) *Transaction {
 	depositFee := TakePercent(amount, feeTax)
 
 	return NewTransaction(nil).
 		WithExternalID(externalID).
-		AddEntry(cashAccountID, Debit, Debit, amount, currency).
-		AddEntry(feeAccount, Credit, Credit, depositFee, currency).
-		AddEntry(toAccountID, Credit, Credit, new(big.Int).Sub(amount, depositFee), currency).
+		AddEntry(debitAccountID, Debit, Debit, amount, currency).
+		AddEntry(creditFeeAccountID, Credit, Credit, depositFee, currency).
+		AddEntry(toCreditAccountID, Credit, Credit, new(big.Int).Sub(amount, depositFee), currency).
 		MustBuild()
 }
 
 // Withdraw creates a transaction for withdrawing an amount from a credit account and a cash account with debit normal side.
-func Withdraw(externalID string, cashAccountID, fromAccount uuid.UUID, amount *big.Int, currency Currency) *Transaction {
+func Withdraw(externalID string, debitAccountID, fromCreditAccountID uuid.UUID, amount *big.Int, currency Currency) *Transaction {
 	return NewTransaction(nil).
 		WithExternalID(externalID).
-		AddEntry(cashAccountID, Credit, Debit, amount, currency).
-		AddEntry(fromAccount, Debit, Credit, amount, currency).
+		AddEntry(debitAccountID, Credit, Debit, amount, currency).
+		AddEntry(fromCreditAccountID, Debit, Credit, amount, currency).
 		MustBuild()
 }
 
 // WithdrawWithFee is the same as Withdraw but takes part of the amount as fee which is transferred to a fee account.
-func WithdrawWithFee(externalID string, cashAccountID, feeAccount, fromAccount uuid.UUID, feeTax int8, amount *big.Int, currency Currency) *Transaction {
+func WithdrawWithFee(externalID string, debitAccountID, creditFeeAccountID, fromCreditAccount uuid.UUID, feeTax int8, amount *big.Int, currency Currency) *Transaction {
 	withdrawFee := TakePercent(amount, feeTax)
 
 	return NewTransaction(nil).
 		WithExternalID(externalID).
-		AddEntry(cashAccountID, Credit, Debit, new(big.Int).Sub(amount, withdrawFee), currency).
-		AddEntry(feeAccount, Credit, Credit, withdrawFee, currency).
-		AddEntry(fromAccount, Debit, Credit, amount, currency).
+		AddEntry(debitAccountID, Credit, Debit, new(big.Int).Sub(amount, withdrawFee), currency).
+		AddEntry(creditFeeAccountID, Credit, Credit, withdrawFee, currency).
+		AddEntry(fromCreditAccount, Debit, Credit, amount, currency).
 		MustBuild()
 }
 
